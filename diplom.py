412f884685691c3ccedb212a14fdc519005d0e5b
Timophey Maiorov <tam92@mail.ru>
2021-07-23 00:45:25 +1000

    Diplom

---
+++ diplom.py
@@ -0,0 +1,72 @@
+﻿
+import requests
+import json
+
+vk_id = input('VK id: ')
+yandex_token = input('Yandex token: ')
+yandex_headers = {
+  "Authorization": "OAuth " + token
+}
+vk_token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
+
+url = "https://api.vk.com/method/photos.get?owner_id=" + vk_id + "&album_id=profile&extended=1&access_token=" + vk_token + "&v=5.131"
+r = requests.get(url)
+
+data = {}
+
+photos_in = r.json()["response"]["items"]
+count = len(photos_in)
+for photo in photos_in:
+  max_size = 0
+  url = ""
+  size_type = ""
+  name = photo["likes"]["count"]
+  for size in photo["sizes"]:
+    if size["width"] * size["height"] > max_size:
+      max_size = size["width"] * size["height"]
+      url = size["url"]
+      size_type = size["type"]
+  if name not in data:
+    data[name] = []
+  data[name].append({
+    "filename": name,
+    "size": size_type,
+    "type": url.split(".")[-1].split("?")[0],
+    "date": photo["date"],
+    "url": url
+  })
+
+print("0/" + str(count))
+photos_out = []
+i = 1
+for likes in data:
+  with_date = False
+  if len(data[likes]) > 1:
+    with_date = True
+  for photo in data[likes]:
+    name = str(likes)
+    if with_date:
+      name = name + "_" + str(photo["date"])
+    name = name + "." + photo["type"]
+
+    folder = "vk"
+    url = "https://cloud-api.yandex.net/v1/disk/resources/?path=" + folder
+    r = requests.put(url, headers=yandex_headers)
+
+    filedata = requests.get(photo["url"])
+    url = "https://cloud-api.yandex.net/v1/disk/resources/upload?path=" + folder + "/" + name + "&overwrite=true"
+    r = requests.get(url, headers=yandex_headers).json()
+    if "href" in r:
+      r = requests.put(r["href"], data=filedata.content, headers=yandex_headers)
+
+    photos_out.append({
+      "file_name": name,
+      "type": photo["size"]
+    })
+
+    print(str(i) + "/" + str(count))
+    i = i + 1
+
+f = open("results.json", "w")
+f.write(json.dumps(photos_out))
+f.close()
